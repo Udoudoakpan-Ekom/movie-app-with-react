0f70528b969fc0db7ec5aa4a5bd97003ef318127
Wizbraindee <64079788+Wizbraindee@users.noreply.github.com>
2022-05-15 21:46:31 +0100

    used API to display more movies and the search button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
 // 82042bb3
 
 const API_URL = "http://www.omdbapi.com?apikey=82042bb3";
+const movie1 = {
+  Title: "Amazing Spiderman Syndrome",
+  Year: "2012",
+  imdbID: "tt2586634",
+  Type: "movie",
+  Poster: "N/A",
+};
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setsearchTerm] = useState("");
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
   useEffect(() => {
     searchMovies("Spiderman");
   }, []);
   return (
     <div className="app">
-      <h1>MovieLand</h1>;
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) =>
+            setsearchTerm(e.target.value)
+          } /*To be able to change the value option instead of making it static */
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
